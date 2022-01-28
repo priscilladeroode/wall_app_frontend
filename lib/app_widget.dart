b93bc29f7729721353f203f71af5fded9edf2b01
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-28 11:56:38 -0300

    first commit

---
+++ lib/app_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wall App',
+      theme: ThemeData(),
+    );
+  }
+}
