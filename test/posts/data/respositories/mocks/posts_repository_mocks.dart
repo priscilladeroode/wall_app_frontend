963410661fe16d94f31aa04b2a4a4aa887a00b3c
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-28 15:45:38 -0300

    test: ensure PostsRepositoryImpl returns a list of PostResponseEntity on success

--- test/posts/data/respositories/mocks/posts_repository_mocks.dart
+++ test/posts/data/respositories/mocks/posts_repository_mocks.dart
@@ -1,5 +1,6 @@
 import 'package:faker/faker.dart';
 import 'package:wall_app_frontend/posts/data/models/post_response_model.dart';
+import 'package:wall_app_frontend/posts/domain/entities/post_response_entity.dart';
 
 final fakeId = faker.lorem.word();
 final fakeTitle = faker.lorem.sentence();
@@ -8,6 +9,14 @@ final fakeCreatedBy = faker.person.name();
 final fakeCreatedAt = DateTime.now();
 
 final postModel = PostResponseModel(
+  id: fakeId,
+  title: fakeTitle,
+  content: fakeContent,
+  createdBy: fakeCreatedBy,
+  createdAt: fakeCreatedAt,
+);
+
+final postEntity = PostResponseEntity(
   id: fakeId,
   title: fakeTitle,
   content: fakeContent,
---
+++ test/posts/data/respositories/posts_repository_impl_test.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:wall_app_frontend/commons/either.dart';
+import 'package:wall_app_frontend/posts/data/datasources/posts_datasource.dart';
+import 'package:wall_app_frontend/posts/data/mappers/post_mapper.dart';
+import 'package:wall_app_frontend/posts/data/respositories/posts_repository_impl.dart';
+import 'package:wall_app_frontend/posts/domain/entities/post_response_entity.dart';
+import 'package:wall_app_frontend/posts/domain/repositories/posts_respository.dart';
+
+import 'mocks/posts_repository_mocks.dart';
+
+class PostsDatasourceMock extends Mock implements PostsDatasource {}
+
+class PostMapperMock extends Mock implements PostMapper {}
+
+void main() {
+  late PostsDatasource datasource;
+  late PostsRepository repository;
+  late PostMapper mapper;
+
+  setUpAll(() {
+    datasource = PostsDatasourceMock();
+    mapper = PostMapperMock();
+    repository = PostsRepositoryImpl(datasource: datasource, mapper: mapper);
+  });
+  test('''
+    Given a valid call for the method loadAll,
+    When datasource return a list of posts,
+    Then a list of PostResponseEntity should be returned.
+  ''', () async {
+    when(() => datasource.loadAll()).thenAnswer((_) async => [postModel]);
+    when(() => mapper.fromModelList(any())).thenReturn([postEntity]);
+
+    final result = await repository.loadAll();
+
+    expect(result.fold(id, id), isA<List<PostResponseEntity>>());
+  });
+}
