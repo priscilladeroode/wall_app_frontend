85efdea3ec4b76bc01be05939aab8bf96e922495
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-29 15:13:59 -0300

    feat: add initial structure of PostDetailsPage

--- lib/posts/posts_module.dart
+++ lib/posts/posts_module.dart
@@ -1,18 +1,25 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:wall_app_frontend/posts/presentation/post_details_page/post_details_page.dart';
 
 import 'data/mappers/post_mapper.dart';
 import 'data/respositories/posts_repository_impl.dart';
 import 'domain/usecases/get_all_posts_usecase.dart';
+import 'domain/usecases/get_post_by_id.dart';
 import 'infra/datasources/posts_datasource_impl.dart';
 import 'presentation/home_page/home_page.dart';
 import 'presentation/home_page/home_page_controller.dart';
 import 'presentation/home_page/stores/home_page_store.dart';
+import 'presentation/post_details_page/post_details_page.dart';
+import 'presentation/post_details_page/post_details_page_controller.dart';
+import 'presentation/post_details_page/stores/post_details_page_store.dart';
 
 class PostsModule extends Module {
   @override
   List<Bind> get binds => [
+        Bind.factory<PostDetailsPageStore>((i) => PostDetailsPageStore()),
+        Bind.factory<PostDetailsPageController>(
+            (i) => PostDetailsPageController(store: i(), usecase: i())),
+        Bind.factory<GetPostById>((i) => GetPostByIdImpl(i())),
         Bind.factory<HomePageStore>((i) => HomePageStore()),
         Bind.factory<HomePageController>((i) => HomePageController(usecase: i(), store: i())),
         Bind.factory<GetAllPostsUseCase>((i) => GetAllPostsUseCaseImpl(i())),
--- lib/posts/presentation/post_details_page/post_details_page.dart
+++ lib/posts/presentation/post_details_page/post_details_page.dart
@@ -1,14 +1,98 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:intl/intl.dart';
+import '../../../commons/breakpoints.dart';
+import '../../../wall_ui.dart/components/wall_app_bar.dart';
+import 'post_details_page_controller.dart';
 
-class PostDetailsPage extends StatelessWidget {
+class PostDetailsPage extends StatefulWidget {
   final String postId;
   const PostDetailsPage({Key? key, required this.postId}) : super(key: key);
 
   @override
+  State<PostDetailsPage> createState() => _PostDetailsPageState();
+}
+
+class _PostDetailsPageState extends ModularState<PostDetailsPage, PostDetailsPageController> {
+  @override
+  void initState() {
+    controller.getPost(widget.postId);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final device = Breakpoint.fromMediaQuery(context).device;
     return Scaffold(
-      body: Center(
-        child: Text(postId),
+      appBar: WallAppBar(
+        device: device,
+        widthScreen: MediaQuery.of(context).size.width,
+      ),
+      body: SingleChildScrollView(
+        child: Observer(builder: (context) {
+          return controller.store.loading
+              ? const Center(child: CircularProgressIndicator())
+              : Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(
+                        left: MediaQuery.of(context).size.width * 0.15,
+                        right: MediaQuery.of(context).size.width * 0.15,
+                        top: 70,
+                        bottom: 16,
+                      ),
+                      child: Text(
+                        controller.store.post?.title ?? '',
+                        style: Theme.of(context).textTheme.headline2,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                        left: MediaQuery.of(context).size.width * 0.15,
+                        right: MediaQuery.of(context).size.width * 0.15,
+                        top: 8,
+                        bottom: 24,
+                      ),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            controller.store.post?.createdBy ?? '',
+                            style: Theme.of(context).textTheme.bodyText1,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 16),
+                            child: Icon(
+                              Icons.circle_rounded,
+                              size: 8,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                          ),
+                          Text(
+                            DateFormat('MMM dd, y')
+                                .format(controller.store.post!.createdAt.toLocal())
+                                .toString(),
+                            style: Theme.of(context).textTheme.bodyText1,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                        left: MediaQuery.of(context).size.width * 0.15,
+                        right: MediaQuery.of(context).size.width * 0.15,
+                        top: 24,
+                        bottom: 40,
+                      ),
+                      child: Text(
+                        controller.store.post?.content ?? '',
+                        style: Theme.of(context).textTheme.bodyText2,
+                      ),
+                    ),
+                  ],
+                );
+        }),
       ),
     );
   }
