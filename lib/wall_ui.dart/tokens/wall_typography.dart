2a0fb1345b951e8f1acfc18d0405520ad977776f
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-29 00:23:27 -0300

    feat: change font because of a bug in the font

--- lib/wall_ui.dart/tokens/wall_typography.dart
+++ lib/wall_ui.dart/tokens/wall_typography.dart
@@ -3,22 +3,24 @@ import 'package:google_fonts/google_fonts.dart';
 
 class WallTypography {
   static TextStyle h1 = GoogleFonts.shadowsIntoLight(fontSize: 72, height: 1.3);
-  static TextStyle h2 = GoogleFonts.openSans(fontSize: 48, height: 1.3);
+  static TextStyle h2 = GoogleFonts.roboto(fontSize: 48, height: 1.3, fontWeight: FontWeight.w600);
   static TextStyle h3 = GoogleFonts.shadowsIntoLight(fontSize: 36, height: 1.3);
-  static TextStyle h4 = GoogleFonts.openSans(fontSize: 32, height: 1.3);
+  static TextStyle h4 = GoogleFonts.roboto(fontSize: 32, height: 1.3, fontWeight: FontWeight.w600);
   static TextStyle h5 = GoogleFonts.shadowsIntoLight(fontSize: 24, height: 1.3);
-  static TextStyle h6 = GoogleFonts.openSans(fontSize: 18, height: 1.3);
+  static TextStyle h6 = GoogleFonts.roboto(fontSize: 18, height: 1.3, fontWeight: FontWeight.w600);
 
-  static TextStyle bodyLarge = GoogleFonts.openSans(fontSize: 16, height: 1.5);
-  static TextStyle bodySmall = GoogleFonts.openSans(fontSize: 14, height: 1.5);
+  static TextStyle bodyLarge =
+      GoogleFonts.roboto(fontSize: 16, height: 1.5, fontWeight: FontWeight.w400);
+  static TextStyle bodySmall =
+      GoogleFonts.roboto(fontSize: 14, height: 1.5, fontWeight: FontWeight.w400);
 
   static TextStyle caption =
-      GoogleFonts.openSans(fontSize: 12, height: 1.6, fontWeight: FontWeight.w600);
+      GoogleFonts.roboto(fontSize: 12, height: 1.6, fontWeight: FontWeight.w600);
 
   static TextStyle elevatedButton =
-      GoogleFonts.openSans(fontSize: 16, height: 1.5, fontWeight: FontWeight.w600);
+      GoogleFonts.roboto(fontSize: 16, height: 1.5, fontWeight: FontWeight.w600);
 
-  static TextStyle textButton = GoogleFonts.openSans(
+  static TextStyle textButton = GoogleFonts.roboto(
     fontSize: 16,
     height: 1.5,
     fontWeight: FontWeight.w600,
