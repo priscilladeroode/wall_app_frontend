21d49e3f2985ba7dedf43ef2ab7696f2bfc7d0f3
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-29 14:06:28 -0300

    test: ensure GetPostByIdImpl returns a PostsFailures when repository returns a failure

--- test/posts/domain/usecases/get_post_by_id_test.dart
+++ test/posts/domain/usecases/get_post_by_id_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:wall_app_frontend/commons/either.dart';
 import 'package:wall_app_frontend/posts/domain/entities/post_response_entity.dart';
+import 'package:wall_app_frontend/posts/domain/failures/posts_failures.dart';
 import 'package:wall_app_frontend/posts/domain/repositories/posts_respository.dart';
 import 'package:wall_app_frontend/posts/domain/usecases/get_post_by_id.dart';
 
@@ -27,5 +28,17 @@ void main() {
     final result = await usecase('');
 
     expect(result.fold(id, id), isA<PostResponseEntity>());
+  });
+
+  test('''
+    Given a valid id,
+    When repository returns a failure,
+    Then should return a failure.
+  ''', () async {
+    when(() => repository.loadById(any())).thenAnswer((_) async => left(PostsRepositoryFailure()));
+
+    final result = await usecase('');
+
+    expect(result.fold(id, id), isA<PostsFailures>());
   });
 }
