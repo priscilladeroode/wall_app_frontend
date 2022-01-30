5c6ab93c747aa5291f7da8b0195b2cc395857f62
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-30 13:40:45 -0300

    chore: removed GoogleFonts package because of bug on web

--- lib/wall_ui.dart/tokens/wall_typography.dart
+++ lib/wall_ui.dart/tokens/wall_typography.dart
@@ -1,26 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class WallTypography {
-  static TextStyle h1 = GoogleFonts.shadowsIntoLight(fontSize: 72, height: 1.3);
-  static TextStyle h2 = GoogleFonts.roboto(fontSize: 48, height: 1.3, fontWeight: FontWeight.w600);
-  static TextStyle h3 = GoogleFonts.shadowsIntoLight(fontSize: 36, height: 1.3);
-  static TextStyle h4 = GoogleFonts.roboto(fontSize: 32, height: 1.3, fontWeight: FontWeight.w600);
-  static TextStyle h5 = GoogleFonts.shadowsIntoLight(fontSize: 24, height: 1.3);
-  static TextStyle h6 = GoogleFonts.roboto(fontSize: 18, height: 1.3, fontWeight: FontWeight.w600);
+  static TextStyle h1 = const TextStyle(fontSize: 72, height: 1.3, fontFamily: 'Shadows');
+  static TextStyle h2 = const TextStyle(fontSize: 48, height: 1.3, fontWeight: FontWeight.w600);
+  static TextStyle h3 = const TextStyle(fontSize: 36, height: 1.3, fontFamily: 'Shadows');
+  static TextStyle h4 = const TextStyle(fontSize: 32, height: 1.3, fontWeight: FontWeight.w600);
+  static TextStyle h5 = const TextStyle(fontSize: 24, height: 1.3, fontFamily: 'Shadows');
+  static TextStyle h6 = const TextStyle(fontSize: 18, height: 1.3, fontWeight: FontWeight.w600);
 
   static TextStyle bodyLarge =
-      GoogleFonts.roboto(fontSize: 16, height: 1.5, fontWeight: FontWeight.w400);
+      const TextStyle(fontSize: 16, height: 1.5, fontWeight: FontWeight.w400);
   static TextStyle bodySmall =
-      GoogleFonts.roboto(fontSize: 14, height: 1.5, fontWeight: FontWeight.w400);
+      const TextStyle(fontSize: 14, height: 1.5, fontWeight: FontWeight.w400);
 
   static TextStyle caption =
-      GoogleFonts.roboto(fontSize: 12, height: 1.6, fontWeight: FontWeight.w600);
+      const TextStyle(fontSize: 12, height: 1.6, fontWeight: FontWeight.w600);
 
   static TextStyle elevatedButton =
-      GoogleFonts.roboto(fontSize: 16, height: 1.5, fontWeight: FontWeight.w600);
+      const TextStyle(fontSize: 16, height: 1.5, fontWeight: FontWeight.w600);
 
-  static TextStyle textButton = GoogleFonts.roboto(
+  static TextStyle textButton = const TextStyle(
     fontSize: 16,
     height: 1.5,
     fontWeight: FontWeight.w600,
