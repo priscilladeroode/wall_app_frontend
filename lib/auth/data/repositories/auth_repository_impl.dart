bb90f4d8a7c9c63e0431b0b65fc58080f97022da
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-30 02:35:22 -0300

    feat: ensure AuthRepositoryImpl return a InvalidName on DioError with errorCode invalid_name

--- lib/auth/data/repositories/auth_repository_impl.dart
+++ lib/auth/data/repositories/auth_repository_impl.dart
@@ -1,3 +1,5 @@
+import 'package:dio/dio.dart';
+
 import '../../../commons/either.dart';
 import '../../domain/entities/signup_request_entity.dart';
 import '../../domain/entities/signup_response_entity.dart';
@@ -24,6 +26,28 @@ class AuthRepositoryImpl implements AuthRepository {
       final _model = mapperFromDomain.handle(signUpCredentials);
       final _result = await datasource.signUp(_model);
       return right(mapperToDomain.fromModel(_result));
+    } on DioError catch (e) {
+      switch (e.response?.data['errorCode']) {
+        case 'invalid_name':
+          return left(InvalidName());
+        case 'invalid_email':
+          return left(InvalidEmail());
+        case 'missing_name':
+          return left(MissingName());
+        case 'missing_email':
+          return left(MissingEmail());
+        case 'missing_password':
+          return left(MissingPassword());
+        case 'missing_passwordConfirmation':
+          return left(MissingPasswordConfirmation());
+        case 'lenght_error_password':
+          return left(InvalidPasswordLength());
+        case 'password_dont_match':
+          return left(PasswordDontMatch());
+        default:
+          return left(
+              AuthRepositoryFailure(message: 'Ops... an error has occured, try again later.'));
+      }
     } on AuthFailures catch (e) {
       return left(e);
     } catch (e) {
--- lib/auth/domain/failures/auth_failures.dart
+++ lib/auth/domain/failures/auth_failures.dart
@@ -15,3 +15,53 @@ class AuthRepositoryFailure extends AuthFailures {
 
   AuthRepositoryFailure({this.message});
 }
+
+class InvalidName extends AuthFailures {
+  @override
+  String? get message => "Your full name must be provided.";
+}
+
+class InvalidPassword extends AuthFailures {
+  @override
+  String? get message => "Invalid password.";
+}
+
+class InvalidPasswordConfirmation extends AuthFailures {
+  @override
+  String? get message => "Invalid password confirmation";
+}
+
+class InvalidEmail extends AuthFailures {
+  @override
+  String? get message => "Invalid email";
+}
+
+class MissingName extends AuthFailures {
+  @override
+  String? get message => "Name can't be empty";
+}
+
+class MissingEmail extends AuthFailures {
+  @override
+  String? get message => "E-mail can't be empty";
+}
+
+class MissingPassword extends AuthFailures {
+  @override
+  String? get message => "Password can't be empty";
+}
+
+class MissingPasswordConfirmation extends AuthFailures {
+  @override
+  String? get message => "Password confirmation can't be empty";
+}
+
+class PasswordDontMatch extends AuthFailures {
+  @override
+  String? get message => "Password and confirmation must match";
+}
+
+class InvalidPasswordLength extends AuthFailures {
+  @override
+  String? get message => "Password must have between 6 and 10 characters";
+}
