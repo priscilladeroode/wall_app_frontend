e7b8bab5a146671a485355dcd3d741c0fcfe5e2e
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-31 14:20:15 -0300

    feat: add LocalStorage dependency injection

--- lib/app_module.dart
+++ lib/app_module.dart
@@ -1,9 +1,14 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'app_controller.dart';
 import 'auth/auth_module.dart';
 import 'auth/presentation/stores/auth_store.dart';
+import 'commons/local_storage/data/mapper/user_from_domain_mapper.dart';
+import 'commons/local_storage/data/repositories/local_storage_repository_impl.dart';
+import 'commons/local_storage/domain/usecases/save_user_usecase.dart';
+import 'commons/local_storage/infra/storages/shared_preferences_storage.dart';
 import 'posts/posts_module.dart';
 import 'splash/splash_module.dart';
 import 'wall_ui.dart/components/wall_app_bar/wall_app_bar_controller.dart';
@@ -15,6 +20,11 @@ class AppModule extends Module {
         Bind.singleton((i) => AuthStore()),
         Bind.lazySingleton((i) => AppController(i())),
         Bind.lazySingleton((i) => WallAppBarController(i())),
+        Bind.factory((i) => SaveUserUseCaseImpl(i())),
+        Bind.factory((i) => LocalStorageRepositoryImpl(i(), i())),
+        Bind.factory((i) => UserFromDomainMapper()),
+        Bind.factory((i) => SharedPreferencesStorage(i())),
+        Bind.lazySingleton((i) => SharedPreferences.getInstance()),
       ];
 
   @override
