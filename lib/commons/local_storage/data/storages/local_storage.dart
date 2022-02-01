1fb2d1caf91fe1bf722886e5867da2c46d01ec20
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-31 21:40:21 -0300

    feat: ensure SharedPreferencesStorage returns a UserModel on getUserInfo success

--- lib/commons/local_storage/data/storages/local_storage.dart
+++ lib/commons/local_storage/data/storages/local_storage.dart
@@ -2,4 +2,5 @@ import '../models/user_model.dart';
 
 abstract class LocalStorage {
   Future<bool> saveUserInfo(UserModel userModel);
+  Future<UserModel?> getUserInfo();
 }
--- lib/commons/local_storage/infra/storages/shared_preferences_storage.dart
+++ lib/commons/local_storage/infra/storages/shared_preferences_storage.dart
@@ -1,29 +1,33 @@
-import 'package:flutter_modular/flutter_modular.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../data/models/user_model.dart';
 import '../../data/storages/local_storage.dart';
 
 class SharedPreferencesStorage implements LocalStorage {
-  SharedPreferences? storage;
-  SharedPreferencesStorage() {
-    init();
-  }
-
-  void init() async {
-    storage ??= await Modular.getAsync<SharedPreferences>();
-  }
-
   @override
   Future<bool> saveUserInfo(UserModel userModel) async {
-    final name = await storage?.setString('name', userModel.name);
-    final email = await storage?.setString('email', userModel.email);
-    final accessToken = await storage?.setString('accessToken', userModel.accessToken);
+    final storage = await SharedPreferences.getInstance();
+    final name = await storage.setString('name', userModel.name);
+    final email = await storage.setString('email', userModel.email);
+    final accessToken = await storage.setString('accessToken', userModel.accessToken);
 
     if (name == true && email == true && accessToken == true) {
       return true;
     } else {
       return false;
+    }
+  }
+
+  @override
+  Future<UserModel?> getUserInfo() async {
+    final storage = await SharedPreferences.getInstance();
+    final name = storage.getString('name');
+    final email = storage.getString('email');
+    final accessToken = storage.getString('accessToken');
+    if (name != null && email != null && accessToken != null) {
+      return UserModel(name: name, email: email, accessToken: accessToken);
+    } else {
+      return null;
     }
   }
 }
