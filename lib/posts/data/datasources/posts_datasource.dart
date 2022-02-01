69f1e7df41b9b892794b6596f0b2e9fb0f394b2f
Priscilla de Roode <priscilladeroode@gmail.com>
2022-02-01 01:53:25 -0300

    feat: ensure PostsDatasourceImpl returns a list of PostResponseModel on loadByUid success

--- lib/posts/data/datasources/posts_datasource.dart
+++ lib/posts/data/datasources/posts_datasource.dart
@@ -3,4 +3,5 @@ import '../models/post_response_model.dart';
 abstract class PostsDatasource {
   Future<List<PostResponseModel>> loadAll();
   Future<PostResponseModel> loadById(String id);
+  Future<List<PostResponseModel>> loadByUid();
 }
--- lib/posts/infra/datasources/posts_datasource_impl.dart
+++ lib/posts/infra/datasources/posts_datasource_impl.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import '../../../auth/presentation/stores/auth_store.dart';
 
 import '../../data/datasources/posts_datasource.dart';
 import '../../data/models/post_response_model.dart';
@@ -6,14 +7,15 @@ import '../../domain/failures/posts_failures.dart';
 
 class PostsDatasourceImpl implements PostsDatasource {
   final Dio dio;
+  final AuthStore _authStore;
 
-  PostsDatasourceImpl(this.dio);
+  PostsDatasourceImpl(this.dio, this._authStore);
 
-  final endpoint = 'http://localhost:5050/api/posts';
+  final endpoint = 'http://10.0.2.2:5050/api';
 
   @override
   Future<List<PostResponseModel>> loadAll() async {
-    final result = await dio.get(endpoint);
+    final result = await dio.get("$endpoint/posts");
     if (result.statusCode == 200) {
       return PostResponseModel.fromJsonList(result.data);
     }
@@ -22,9 +24,21 @@ class PostsDatasourceImpl implements PostsDatasource {
 
   @override
   Future<PostResponseModel> loadById(String id) async {
-    final result = await dio.get('$endpoint/$id');
+    final result = await dio.get('$endpoint/posts/$id');
     if (result.statusCode == 200) {
       return PostResponseModel.fromJson(result.data);
+    }
+    throw PostsDatasourceError();
+  }
+
+  @override
+  Future<List<PostResponseModel>> loadByUid() async {
+    final result = await dio.get('$endpoint/postsByUser',
+        options: Options(headers: {
+          "x-access-token": _authStore.accessToken,
+        }));
+    if (result.statusCode == 200) {
+      return PostResponseModel.fromJsonList(result.data);
     }
     throw PostsDatasourceError();
   }
