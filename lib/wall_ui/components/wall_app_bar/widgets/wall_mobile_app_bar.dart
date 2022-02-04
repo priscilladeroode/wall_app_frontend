28cbd46054f82fb33c5b2ecd39067a84a90e3cf1
Priscilla de Roode <priscilladeroode@gmail.com>
2022-02-03 21:00:30 -0300

    refactor: remove unused code

--- lib/wall_ui/components/wall_app_bar/widgets/wall_mobile_app_bar.dart
+++ lib/wall_ui/components/wall_app_bar/widgets/wall_mobile_app_bar.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import '../wall_app_bar_controller.dart';
 import 'wall_app_bar_logo.dart';
@@ -19,28 +18,26 @@ class WallMobileAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Observer(builder: (context) {
-      return AppBar(
-        toolbarHeight: height,
-        title: SizedBox(
-          width: width,
-          child: const WallAppBarLogo(),
-        ),
-        actions: hideActions
-            ? null
-            : controller.authStore.accessToken == null
-                ? ([
-                    IconButton(
-                        onPressed: () => Modular.to.pushNamed('/auth/signin'),
-                        icon: const Icon(Icons.face))
-                  ])
-                : ([
-                    IconButton(
-                      onPressed: () => controller.signOut(),
-                      icon: const Icon(Icons.exit_to_app),
-                    )
-                  ]),
-      );
-    });
+    return AppBar(
+      toolbarHeight: height,
+      title: SizedBox(
+        width: width,
+        child: const WallAppBarLogo(),
+      ),
+      actions: hideActions
+          ? null
+          : controller.authStore.accessToken == null
+              ? ([
+                  IconButton(
+                      onPressed: () => Modular.to.pushNamed('/auth/signin'),
+                      icon: const Icon(Icons.face))
+                ])
+              : ([
+                  IconButton(
+                    onPressed: () => controller.signOut(),
+                    icon: const Icon(Icons.exit_to_app),
+                  )
+                ]),
+    );
   }
 }
